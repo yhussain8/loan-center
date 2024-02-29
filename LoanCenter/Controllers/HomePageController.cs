3c5b34aa8b1db033731fe87e4ec5e80d418c7942
Yousuf Hussain <yhussain8@gmail.com>
2024-02-29 01:35:33 -0500

    Create HomePageController with "hello world" message to "/index" route

---
+++ LoanCenter/Controllers/HomePageController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoanCenter.Controllers
+{
+    [Route("/index")]
+    [ApiController]
+    public class HomePageController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Hello Nick!");
+        }
+    }
+}
