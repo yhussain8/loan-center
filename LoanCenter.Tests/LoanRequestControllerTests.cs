6e9e31364ccfe388cc7841e0933ede9758b86016
Yousuf Hussain <yhussain8@gmail.com>
2024-02-29 02:37:12 -0500

    Create controller and unit test for LoanRequest endpoint

---
+++ LoanCenter.Tests/LoanRequestControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using LoanCenter.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoanCenter.Tests
+{
+    public class LoanRequestControllerTests
+    {
+        [Fact]
+        public void LoanRequest_ReturnsStatusCode201()
+        {
+            var controller = new LoanRequestController();
+
+            var testEmailAddress = "nick@coach.com";
+            var result = controller.LoanRequest(testEmailAddress);
+
+            var statusCodeResult  = Assert.IsType<StatusCodeResult>(result);
+            Assert.Equal(201, statusCodeResult.StatusCode);
+        }
+    }
+}
---
+++ LoanCenter/Controllers/LoanRequestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoanCenter.Controllers
+{
+    [Route("/loanRequest")]
+    [ApiController]
+    public class LoanRequestController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult LoanRequest(string emailAddress)
+        {
+            Console.WriteLine(emailAddress);
+            return StatusCode(201);
+        }
+    }
+}
